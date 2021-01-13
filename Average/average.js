44afdd66646b2af0da3fdd55037dadc7171ef698
Khan Claudette <khanclaudette2@gmail.com>
2021-01-13 23:28:30 +0100

    Day 5

--- Average/average.js
+++ Average/average.js
@@ -10,3 +10,41 @@ console.log('The length of the array is ' + numberCount);
 
 var average = totalSum / numberCount; //the sum of the Array divided by the length gives the average
 console.log('The average of 5 numbers is ' + average);
+
+
+ 
+class vehicle {
+    color;
+    model;
+    fuel;
+    weight;
+    name;
+    size;
+    pdtDate;
+    speed;
+ 
+    constructor(){
+        this.startCar();
+    }
+    destructor() {
+        this.stopCar()
+    }
+
+    startCar() {
+        console.log('the car has started');
+        sessionStorage.setItem('start', startCar);
+    }
+    stopCar(){
+        console.log('the car has stoped');
+        sessionStorage.setItem('stop', this.stopCar)
+    }
+}
+
+var vowels = "aeiou";
+var label = "banana";
+
+var stripped = "";
+for (i = 0; i < label.length; i++) {
+  if(vowels.indexOf(label[i]) >= 0) stripped += label[i];
+}
+console.log(stripped);
